a32b54054d89e018ddfc2315415b1e565058c3fd
Suman_Godua <mr.sumangouda@gmail.com>
2026-04-24 13:08:51 +0530

    Updated the scraper function and reorganized the folder structure

--- scraper/scraper.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from emotion.emotion_extractor import get_emotions_from_storyline
-
-print("Hello Neemo")
